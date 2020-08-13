7c8082a7abd261724e959a184e2f8fe183ae6fdc
Sean Wilson <paintballsean@gmail.com>
2020-08-13 16:34:37 -0700

    Commit makes all else statements working except final error.
    Co-authored-by: Daniel Schaaf <daniel.schaaf@outlook.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,23 @@
-$(document).ready(function() {
-  $("#triangle").submit(function(event) {
+$(document).ready(function () {
+  $("#triangle").submit(function (event) {
+    $('#equalTriangle, #isoTriangle, #scaleTriangle, #noTriangle, #noData').hide();
     const sideA = parseInt($("#sideA").val());
     const sideB = parseInt($("#sideB").val());
     const sideC = parseInt($("#sideC").val());
     event.preventDefault();
-    
-    if (sideA == sideB == sideC) {
-      $("#equalTriangle").show();
-    } 
-    else if ( sideA == sideB && sideA !=sideC || sideB==sideC && sideB != sideA){ 
-      $("#isoTriangle").show();
-    } 
-    else if (sideA != sideB !=sideC ) { 
-      $("#scaleTriangle").show();
-    } 
-    else {
-      $("#noTriangle").show();
-    }
-  }); 
-}); 
 
+    if (sideA + sideB <= sideC || sideB + sideC <= sideA || sideA + sideC <= sideB) {
+      $("#noTriangle").show();
+    } else if (sideA === sideB || sideA === sideC || sideB === sideC) {
+      $("#isoTriangle").show();
+    } else if ((sideA !== sideB) && (sideA !== sideC) && (sideB !== sideC)) {
+      $("#scaleTriangle").show();
+    } else if (sideA === sideB && sideA === sideC) {
+      $("#equalTriangle").show();
+    }
+    
+    else {
+      $("#noData").show();
+    }
+  });
+});
